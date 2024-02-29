f65d829a3bf6ca6c487ef6476360073b1e509f73
Leandro27007 <125936141+Leandro27007@users.noreply.github.com>
2024-02-29 11:34:53 -0400

    Layout Inicial, Guards y login

--- LaboratorioOnline/src/app/app.routes.ts
+++ LaboratorioOnline/src/app/app.routes.ts
@@ -1,14 +1,33 @@
 import { Routes } from '@angular/router';
 
+import { LayoutComponent } from './pages/layout/layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { authGuard } from './service/auth/auth.guard';
+import { LoginComponent } from './pages/login/login.component';
+import ResultadoComponent from './pages/resultado/resultado.component';
+
 export const routes: Routes = [
     {
-        path: 'login',
-        title: 'Iniciar Sesion',
-        loadComponent: () => import('./pages/login/login.component'),
+        path: '', redirectTo:'login' , pathMatch:'full'
     },
     {
-        path: 'resultado',
-        title: 'Iniciar Sesion',
-        loadComponent: () => import('./pages/resultado/resultado.component'),
+        path:'login',
+        component:LoginComponent
     },
+    {
+        path:'',
+        component:LayoutComponent,
+        children:[
+            {
+                path:'dashboard',
+                component:DashboardComponent,
+                canActivate: [authGuard]
+            },
+            {
+                path:'resultado',
+                component:ResultadoComponent,
+                canActivate: [authGuard]
+            }
+        ]
+    }
 ];
--- LaboratorioOnline/src/app/pages/login/login.component.ts
+++ LaboratorioOnline/src/app/pages/login/login.component.ts
@@ -1,22 +1,52 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export default class LoginComponent {
+export class LoginComponent implements OnInit{
 
-  users = [{ nombre: 'lucas' }, { nombre: 'juan' }, { nombre: 'pedro' }]
+  loginObj: Login;
 
-  Login() {
+  constructor(private http: HttpClient,private router: Router) {
+    this.loginObj = new Login();
+  }
+  ngOnInit(): void {
+    const token = localStorage.getItem('angular17token');
 
-
+    if(token)
+      this.router.navigateByUrl('/dashboard')
 
   }
 
+
+  onLogin() {
+    debugger;
+    this.http.post('https://localhost:7290/api/Autenticacion/Login', this.loginObj).subscribe((res:any)=>{
+      
+    if(res.token) {
+        alert("Login Success");
+        localStorage.setItem('angular17token', res.token)
+        this.router.navigateByUrl('/dashboard')
+      } else {
+        alert(res.message)
+      }
+    })
+  }
+}
+
+export class Login { 
+    userName: string;
+    password: string;
+    constructor() {
+      this.userName = '';
+      this.password = '';
+    } 
 }
