71a28c7575c8ebc04780f644b9a57ea2ff0637e4
Masuel Matos <masuelm04@gmail.com>
2024-02-29 15:22:59 -0400

    navbar cambiada

--- LaboratorioOnline/src/app/pages/layout/layout.component.ts
+++ LaboratorioOnline/src/app/pages/layout/layout.component.ts
@@ -1,18 +1,27 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { Router,RouterLink,RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
 export class LayoutComponent {
 
   constructor(private router: Router) {
-
   }
+  showMenu: boolean = false;
+
+  ngOnInit(): void {
+  }
+
+  toggleMenu() {
+    this.showMenu = !this.showMenu;
+  }
+
 
   logoff() {
     localStorage.removeItem('angular17token')
